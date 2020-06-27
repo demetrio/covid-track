8e129e8b1fc784bcd20afcabf38b8639e29f1ab8
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2020-06-27 23:36:47 +0200

    Added Card
    Card completed

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import styles from './Chart.modules.css';
+import { Line, Bar } from 'react-chartjs-2';
 
 const Chart = () => {
+  const [dailyData, setDailyData] = useState({});
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    fetchAPI();
+  });
+
   return <div>This is a Chart</div>;
 };
 
