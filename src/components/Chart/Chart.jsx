313ef5bb70c5e09dc63baf357e81d07382413c1c
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2020-11-22 23:57:19 +0100

    Refactor Chart.js to simplify the Component

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -14,49 +14,51 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     fetchAPI();
   }, []);
 
+  const LineDataset = [
+    {
+      data: dailyData.map(data => data.confirmed),
+      label: 'Infected',
+      borderColor: '#3333ff',
+      fill: true,
+      lineTension: 0.1,
+      borderCapStyle: 'butt',
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(51,51,255,0.5)',
+      pointBackgroundColor: '#3333ff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(51,51,255,0.5)',
+      pointHoverBorderColor: 'rgba(51,51,255,0.5)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+    },
+    {
+      data: dailyData.map(data => data.deaths),
+      label: 'Deaths',
+      borderColor: 'red',
+      backgroundColor: 'rgba(255, 0, 0, 0.5)',
+      fill: true,
+      lineTension: 0.1,
+      borderCapStyle: 'butt',
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(255, 0, 0, 0.5)',
+      pointBackgroundColor: 'red',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(255, 0, 0, 0.5)',
+      pointHoverBorderColor: 'rgba(255, 0, 0, 0.5)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+    },
+  ];
+
   const lineChart = dailyData[0] ? (
     <Line
       data={{
         labels: dailyData.map(({ date }) => date),
-        datasets: [
-          {
-            data: dailyData.map(data => data.confirmed),
-            label: 'Infected',
-            borderColor: '#3333ff',
-            fill: true,
-            lineTension: 0.1,
-            borderCapStyle: 'butt',
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(51,51,255,0.5)',
-            pointBackgroundColor: '#3333ff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(51,51,255,0.5)',
-            pointHoverBorderColor: 'rgba(51,51,255,0.5)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-          },
-          {
-            data: dailyData.map(data => data.deaths),
-            label: 'Deaths',
-            borderColor: 'red',
-            backgroundColor: 'rgba(255, 0, 0, 0.5)',
-            fill: true,
-            lineTension: 0.1,
-            borderCapStyle: 'butt',
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(255, 0, 0, 0.5)',
-            pointBackgroundColor: 'red',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(255, 0, 0, 0.5)',
-            pointHoverBorderColor: 'rgba(255, 0, 0, 0.5)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-          },
-        ],
+        datasets: LineDataset,
       }}
     />
   ) : null;
